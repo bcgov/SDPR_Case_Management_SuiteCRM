1bf93799751c982ad0d3b59b7c06efb61b73beca
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2024-06-17 00:30:16 -0700

    fix: suitecrm customizations

---
+++ docker/build/suitecrm/public/legacy/custom/Extension/modules/Cases/Ext/Vardefs/_override_sugarfield_advocase_case_issue_2_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2024-06-14 18:37:51
+$dictionary['Case']['fields']['advocase_case_issue_2_c']['inline_edit']='1';
+$dictionary['Case']['fields']['advocase_case_issue_2_c']['labelValue']='Issue 2';
+
+ ?>
--- docker/build/suitecrm/public/legacy/custom/Extension/modules/Cases/Ext/Vardefs/_override_sugarfield_advocase_case_other_issue_c.php
+++ docker/build/suitecrm/public/legacy/custom/Extension/modules/Cases/Ext/Vardefs/_override_sugarfield_advocase_case_other_issue_c.php
@@ -1,6 +1,6 @@
 <?php
- // created: 2024-05-18 00:13:44
+ // created: 2024-06-14 18:39:29
 $dictionary['Case']['fields']['advocase_case_other_issue_c']['inline_edit']='1';
-$dictionary['Case']['fields']['advocase_case_other_issue_c']['labelValue']='Other issue';
+$dictionary['Case']['fields']['advocase_case_other_issue_c']['labelValue']='If other, please specify';
 
  ?>
