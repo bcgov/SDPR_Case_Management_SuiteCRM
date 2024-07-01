c18f6ad26075666e49031f0b52d67db15d472d4b
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2024-06-30 20:20:55 -0700

    ASA-151/rs fix: cron script replacing right string

--- docker/advocase-image/scripts/cron.sh
+++ docker/advocase-image/scripts/cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -i "s/0 => 'some other root cron',/0 => '$(whoami)',/g" /suitecrm/public/legacy/config.php
+sed -i "s/0 => 'root_REMOVE_THIS_NOTICE_IF_YOU_REALLY_WANT_TO_ALLOW_ROOT',/0 => '$(whoami)',/g" /suitecrm/public/legacy/config.php
 
 php -f /suitecrm/public/legacy/cron.php > /dev/null 2>&1
