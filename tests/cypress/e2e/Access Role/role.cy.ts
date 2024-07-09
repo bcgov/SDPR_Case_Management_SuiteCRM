b278373e5d92b875f68436809f2d1be09fb669b9
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2024-07-08 21:50:44 -0700

    refactor: update endpoint to UAT environment

--- tests/cypress/e2e/Access Role/role.cy.ts
+++ tests/cypress/e2e/Access Role/role.cy.ts
@@ -3,7 +3,7 @@ import 'cypress-iframe'
 describe('ASA 68 - Assign Roles', () => {
   describe('ASA 119 - Create role', () => {
     it('Should create a new role', () => {
-      cy.visit('https://advocase-d0d1b5-dev.apps.gold.devops.gov.bc.ca/#/acl-roles/edit')
+      cy.visit('https://advocase-d0d1b5-test.apps.gold.devops.gov.bc.ca/#/acl-roles/edit')
       cy.wait(10000)
 
       cy.frameLoaded('iframe')
@@ -18,7 +18,7 @@ describe('ASA 68 - Assign Roles', () => {
 
   describe('ASA 120 - Update role', () => {
     beforeEach(() => {
-      cy.visit('https://advocase-d0d1b5-dev.apps.gold.devops.gov.bc.ca/#/acl-roles/index')
+      cy.visit('https://advocase-d0d1b5-test.apps.gold.devops.gov.bc.ca/#/acl-roles/index')
       cy.wait(10000)
       cy.get('scrm-dynamic-field > a', { timeout: 10000 })
         .first()
