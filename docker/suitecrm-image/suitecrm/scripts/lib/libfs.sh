90a087dfe9cfdbbee72d1eede772b019a88066c5
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2024-07-09 19:52:43 -0700

    ASA-108/rs feat: libfs.sh

---
+++ docker/suitecrm-image/suitecrm/scripts/lib/libfs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Author: Raphael Cássio de Souza (rsouza@nautilusdigital.com)
+# Description: This script contains the copy files function used in the SuiteCRM scripts
+# Created on: 2024-07-09
+
+########################
+# Copy files from source directory to destination directory
+# Arguments:
+#   $1: Source directory
+#   $2: Destination directory
+# Returns:
+#   None
+#########################
+function copy_files() {
+  local source_dir="$1"
+
+  info "Copying files from $source_dir to $destination_dir";
+
+  for FILE in $(find "$source_dir" -type f); do
+    if [[ -f "$FILE" ]]; then
+      local dest_file
+
+      if [[ -z ${2} ]]; then
+        dest_file="${FILE#${source_dir}}"
+      else
+        dest_file="$2${FILE#${source_dir}}"
+      fi
+
+      local dest_dir=$(dirname "$dest_file")
+      create_dir_if_not_exists "$dest_dir"
+
+      info "Copying $FILE to $dest_file";
+      cp "$FILE" "$dest_file"
+    fi
+  done
+}
+
+function create_dir_if_not_exists() {
+  local dir="$1"
+
+  if [[ ! -d "$dir" ]]; then
+    info "Creating directory $dir";
+    mkdir -p "$dir"
+  fi
+}
