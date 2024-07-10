b735b4ba818125b7acba67b019b0d17277b36d17
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2024-07-09 19:53:05 -0700

    ASA-108/rs feat: entrypoint.sh

---
+++ docker/suitecrm-image/suitecrm/scripts/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Author: Raphael Cássio de Souza (rsouza@nautilusdigital.com)
+# Description: Docker entrypoint script for BC GOV SuiteCRM Docker image
+# Created on: 2024-07-09
+
+RUN_DIR=${RUN_DIR:-"/opt/suitecrm/scripts"}
+
+export RUN_DIR
+
+# Load the libraries
+. $RUN_DIR/lib/liblog.sh
+. $RUN_DIR/lib/libfs.sh
+
+# Validate the number of arguments
+if [ "$#" -eq 0 ]; then
+  error "entrypoint.sh script requires at least the source path argument"
+  exit 1
+fi
+
+info "Starting SuiteCRM entrypoint script"
+copy_files "$1" "$2"
