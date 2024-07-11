cec3c3ae7b0f8509f64784fc6fcb3f6d3c31e80f
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2024-07-10 23:52:01 -0700

    feat: accessibility check on test pages

--- tests/cypress/e2e/Access Role/role.cy.ts
+++ tests/cypress/e2e/Access Role/role.cy.ts
@@ -1,60 +1,75 @@
 import 'cypress-iframe'
+import 'cypress-axe'
+import { terminalLog } from '../utils'
 
 describe('ASA 68 - Assign Roles', {
   env: {
-    route: `${Cypress.env('scrm_UAT')}/#acl-roles`
+    route: '/#acl-roles'
   }
 }, () => {
   describe('ASA 119 - Create role', () => {
-    it('Should create a new role', () => {
-      cy.visit(`${Cypress.env('route')}/edit`)
-      cy.wait(10000)
-
+    beforeEach(() => {
+      cy.visit(`/${Cypress.env('route')}/edit`)
+      cy.wait(8000)
       cy.frameLoaded('iframe')
+    })
+
+    it('Should create a new role', () => {
       cy.iframe().find('input[id="name"]').type("ASA-119")
       cy.iframe().find('textarea[name="description"]').type("Role creation through test automation")
-      cy.wait(10000)
+      cy.wait(6000)
 
       cy.frameLoaded('iframe')
       cy.iframe().find('input[title="Save"]').click()
+
+      cy.checkA11y(null, null, terminalLog)
     })
   })
 
   describe('ASA 120 - Update role', () => {
     beforeEach(() => {
-      cy.visit(`${Cypress.env('route')}/index`)
+      cy.visit(`/${Cypress.env('route')}/index`)
+      cy.injectAxe()
+      cy.checkA11y(null, null, terminalLog)
       cy.wait(10000)
       cy.get('scrm-dynamic-field > a', { timeout: 10000 })
         .first()
         .click()
+      cy.checkA11y(null, null, terminalLog)
       cy.wait(5000)
 
       cy.frameLoaded('iframe')
     })
 
     it('Should update role name', () => {
-      cy.wait(10000)
+      cy.wait(3000)
       cy.iframe().find('input[title="Edit"]').click()
-      cy.wait(5000)
+      cy.wait(3000)
       cy.frameLoaded('iframe')
       cy.iframe().find('input[id="name"]').type("ASA-120")
       cy.iframe().find('input[id="save_button"]').click()
+
+      cy.checkA11y(null, null, terminalLog)
     })
 
     it('Should update role description', () => {
-      cy.wait(10000)
+      cy.wait(3000)
       cy.iframe().find('input[title="Edit"]').click()
-      cy.wait(5000)
+      cy.wait(3000)
       cy.frameLoaded('iframe')
       cy.iframe().find('textarea[name="description"]').type("ASA-120")
       cy.iframe().find('input[id="save_button"]').click()
       cy.iframe().find('input[title="Save"]').click()
+
+      cy.checkA11y(null, null, terminalLog)
     })
 
     it('Should update role permissions', () => {
       cy.wait(10000)
       cy.iframe().find('div[id="viewlink"]').click()
       cy.iframe().find('select[id="act_guidview"]').select('All')
+
+      cy.checkA11y(null, null, terminalLog)
     })
   })
 })
