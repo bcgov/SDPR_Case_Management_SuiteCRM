d1f17fd1cd4962bae74d6c96898726bbdd15a989
tamaradesmith <tdesmith81@gmail.com>
2024-07-15 15:19:27 -0700

    Feat/asa-233: contant subpanel Tables

---
+++ docker/advocase-image/build-files/suitecrm/public/legacy/modules/Contacts/metadata/subpanels/Contact_subpanel_contacts.php
@@ -0,0 +1,86 @@
+<?php
+// created: 2024-07-15 19:47:44
+$subpanel_layout['list_fields'] = array (
+  'name' =>
+  array (
+    'name' => 'name',
+    'vname' => 'LBL_LIST_NAME',
+    'widget_class' => 'SubPanelDetailViewLink',
+    'module' => 'Contacts',
+    'width' => '23%',
+    'default' => true,
+  ),
+  'type_of_contact_c' =>
+  array (
+    'type' => 'enum',
+    'default' => true,
+    'studio' => 'visible',
+    'vname' => 'LBL_TYPE_OF_CONTACT',
+    'width' => '10%',
+  ),
+  'primary_address_city' =>
+  array (
+    'type' => 'varchar',
+    'vname' => 'LBL_LIST_PRIMARY_ADDRESS_CITY',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'advocase_contact_region_c' =>
+  array (
+    'type' => 'varchar',
+    'default' => true,
+    'vname' => 'LBL_LIST_ADVOCASE_CONTACT_REGION_C',
+    'width' => '10%',
+  ),
+  'phone_work' =>
+  array (
+    'name' => 'phone_work',
+    'vname' => 'LBL_LIST_PHONE',
+    'width' => '15%',
+    'default' => true,
+  ),
+  'email1' =>
+  array (
+    'name' => 'email1',
+    'vname' => 'LBL_LIST_EMAIL',
+    'widget_class' => 'SubPanelEmailLink',
+    'width' => '30%',
+    'sortable' => false,
+    'default' => true,
+  ),
+  'edit_button' =>
+  array (
+    'vname' => 'LBL_EDIT_BUTTON',
+    'widget_class' => 'SubPanelEditButton',
+    'module' => 'Contacts',
+    'width' => '5%',
+    'default' => true,
+  ),
+  'remove_button' =>
+  array (
+    'vname' => 'LBL_REMOVE',
+    'widget_class' => 'SubPanelRemoveButton',
+    'module' => 'Contacts',
+    'width' => '5%',
+    'default' => true,
+  ),
+  'first_name' =>
+  array (
+    'name' => 'first_name',
+    'usage' => 'query_only',
+  ),
+  'last_name' =>
+  array (
+    'name' => 'last_name',
+    'usage' => 'query_only',
+  ),
+  'salutation' =>
+  array (
+    'name' => 'salutation',
+    'usage' => 'query_only',
+  ),
+  'contact_id' =>
+  array (
+    'usage' => 'query_only',
+  ),
+);
---
+++ docker/advocase-image/build-files/suitecrm/public/legacy/modules/Documents/Ext/Language/en_us.lang.ext.php
@@ -0,0 +1,7 @@
+<?php
+// WARNING: The contents of this file are auto-generated
+
+
+// created: 2024-07-15 16:29:53
+$mod_strings['LBL_FILENAME'] = 'Attached File Name';
+
