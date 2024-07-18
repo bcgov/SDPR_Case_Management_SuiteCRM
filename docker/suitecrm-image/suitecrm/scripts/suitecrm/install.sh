12a4c8793bf40e994a3c578b2fa5807bf3c64ae0
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2024-07-18 12:54:54 -0700

    ASA-108/rs feat: Restore or install script

--- docker/suitecrm-image/suitecrm/scripts/suitecrm/install.sh
+++ docker/suitecrm-image/suitecrm/scripts/suitecrm/install.sh
@@ -12,7 +12,7 @@
 ########################
 # Install SuiteCRM
 # Arguments:
-#   Extension list
+#   None
 # Returns:
 #   None
 #########################
@@ -23,6 +23,52 @@ function install_suitecrm() {
   # we access the SuiteCRM URL. So, we need to access the SuiteCRM URL first
   curl -S localhost:8181/install.php
 
+  suitecrm_install_or_rebuild
+}
+
+########################
+# Install or Rebuild SuiteCRM
+# Arguments:
+#   None
+# Returns:
+#   None
+#########################
+function suitecrm_install_or_rebuild() {
+  info "Checking if SuiteCRM is already installed"
+
+  info "Running query to check if SuiteCRM is already installed"
+  local query_result=$(echo "SELECT COUNT(*) FROM users" | mariadb -s -N --host $SUITE_DB_HOST --port $SUITE_DB_PORT --user $SUITE_DB_USER --database $SUITE_DB_NAME --password=$SUITE_DB_PASSW)
+
+  if [[ $query_result -ge 1 ]]; then
+    info "SuiteCRM is already installed"
+
+    info "Copying SuiteCRM temporary configuration file"
+    cp /opt/suitecrm/scripts/suitecrm/php/config-db-tpl.php $SUITECRM_DIR/public/legacy/config.php
+    
+    # Remove .env.local file
+    info "Removing .env.local file"
+    rm $SUITECRM_DIR/.env.local
+
+    # Clear cache
+    info "Clearing SuiteCRM cache"
+    rm -r cache/* public/legacy/cache/*
+  else
+    info "SuiteCRM is not installed"
+
+    suitecrm_install_command
+  fi
+
+  suitecrm_rebuild_files
+}
+
+########################
+# Install SuiteCRM command
+# Arguments:
+#   None
+# Returns:
+#   None
+#########################
+function suitecrm_install_command() {
   info "Running SuiteCRM installation command"
   $SUITECRM_DIR/bin/console suitecrm:app:install \
                 -u "admin" \
@@ -33,37 +79,24 @@ function install_suitecrm() {
                 -N $SUITE_DB_NAME \
                 -S $SUITECRM_URL \
                 -d "no"
-
-  # Remove .env.local file
-  info "Removing .env.local file"
-  rm $SUITECRM_DIR/.env.local
-
-  # Clear cache
-  info "Clearing SuiteCRM cache"
-  rm -r cache/* public/legacy/cache/*
-
-  # Rebuild SuiteCRM's configuration file
-  suitecrm_rebuild_files
-  if [[ $? -ne 0 ]]; then
-    error "Failed to rebuild SuiteCRM configuration file"
-    return 1
-  fi
 }
 
 ########################
 # Rebuild SuiteCRM's configuration file
 # Globals:
-#   *
+#   SUITECRM_DIR: SuiteCRM installation directory
 # Arguments:
 #   None
 # Returns:
-#   true if succeded, false otherwise
+#   None
 #########################
-suitecrm_rebuild_files() {
-    # The below script executes the code from "Repair > Rebuild Config File" to regenerate the configuration file
-    # We prefer to run a script rather than via cURL requests because it would require to login, and could cause
-    # issues with SUITECRM_SKIP_BOOTSTRAP
-    php_execute <<EOF
+function suitecrm_rebuild_files() {
+  # The below script executes the code from "Repair > Rebuild Config File" to regenerate the configuration file
+  # We prefer to run a script rather than via cURL requests because it would require to login, and could cause
+  # issues with SUITECRM_SKIP_BOOTSTRAP
+
+  info "Rebuilding SuiteCRM configuration file"
+  php_execute <<EOF
 chdir('$SUITECRM_DIR/public/legacy');
 define('sugarEntry', true);
 require_once('include/utils.php');
@@ -86,11 +119,18 @@ require_once('include/utils/file_utils.php');
 // Based on the RebuildConfig action in the admin panel
 \$clean_config = loadCleanConfig();
 rebuildConfigFile(\$clean_config, \$sugar_version);
-
-// Rebuild the .htaccess file
-// Based on the UpgradeAccess action in the admin panel
-// require_once 'include/upload_file.php';
-// UploadStream::register();
-// require('modules/Administration/UpgradeAccess.php');
 EOF
+
+  if [[ $? -ne 0 ]]; then
+    error "Failed to rebuild SuiteCRM configuration file"
+    exit 1
+  fi
+
+  info "Executing Quick Repair and Rebuild script"
+  php_execute /opt/suitecrm/scripts/suitecrm/php/rebuild-suitecrm.php --instance /suitecrm/public/legacy
+
+  if [[ $? -ne 0 ]]; then
+    error "Failed to run Quick Repair and Rebuild script"
+    exit 1
+  fi
 }
