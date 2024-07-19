7ad087ea38ed4c097d1317c52078ee114952a820
tamaradesmith <tdesmith81@gmail.com>
2024-07-19 11:14:35 -0700

    fix/asa-251: meeting columns headers names

---
+++ docker/advocase-image/build-files/suitecrm/public/legacy/custom/modules/Meetings/language/en_us.lang.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_LIST_RELATED_TO' => 'Cases',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Assigned To',
+);
