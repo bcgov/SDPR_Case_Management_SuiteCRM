13a9bf043311bae0fa537aefa8183adba5602dba
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2024-07-19 11:25:16 -0700

    ASA-227 fix: loading entrypoint on CRON script

--- docker/suitecrm-image/suitecrm/scripts/suitecrm/cron.sh
+++ docker/suitecrm-image/suitecrm/scripts/suitecrm/cron.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Load entrypoint script
+. /opt/suitecrm/scripts/entrypoint.sh
+
 sed -i "s/0 => 'root_REMOVE_THIS_NOTICE_IF_YOU_REALLY_WANT_TO_ALLOW_ROOT',/0 => '$(whoami)',/g" /suitecrm/public/legacy/config.php
 
 php -f /suitecrm/public/legacy/cron.php > /dev/null 2>&1
