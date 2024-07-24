29f5aafd754e30916f87c34201cfe4fa5beb2b92
tamaradesmith <tdesmith81@gmail.com>
2024-07-24 09:14:54 -0700

    fix/asa-254: update subpanel table style

---
+++ docker/advocase-image/build-files/suitecrm/public/legacy/custom/modules/Contacts/metadata/subpanels/Contact_subpanel_contacts.php
@@ -0,0 +1,85 @@
+<?php
+// created: 2024-07-24 15:14:38
+$subpanel_layout['list_fields'] = array (
+  'name' => 
+  array (
+    'name' => 'name',
+    'vname' => 'LBL_LIST_NAME',
+    'widget_class' => 'SubPanelDetailViewLink',
+    'module' => 'Contacts',
+    'width' => '23%',
+    'default' => true,
+  ),
+  'advocase_contact_type_c' => 
+  array (
+    'type' => 'enum',
+    'default' => true,
+    'studio' => 'visible',
+    'vname' => 'LBL_ADVOCASE_CONTACT_TYPE',
+    'width' => '10%',
+  ),
+  'primary_address_city' => 
+  array (
+    'type' => 'varchar',
+    'vname' => 'LBL_PRIMARY_ADDRESS_CITY',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'advocase_contact_region_c' => 
+  array (
+    'type' => 'enum',
+    'default' => true,
+    'studio' => 'visible',
+    'vname' => 'LBL_ADVOCASE_CONTACT_REGION',
+    'width' => '10%',
+  ),
+  'phone_work' => 
+  array (
+    'name' => 'phone_work',
+    'vname' => 'LBL_LIST_PHONE',
+    'width' => '15%',
+    'default' => true,
+  ),
+  'advocase_email_c' => 
+  array (
+    'type' => 'varchar',
+    'default' => true,
+    'vname' => 'LBL_ADVOCASE_EMAIL_C',
+    'width' => '10%',
+  ),
+  'edit_button' => 
+  array (
+    'vname' => 'LBL_EDIT_BUTTON',
+    'widget_class' => 'SubPanelEditButton',
+    'module' => 'Contacts',
+    'width' => '5%',
+    'default' => true,
+  ),
+  'remove_button' => 
+  array (
+    'vname' => 'LBL_REMOVE',
+    'widget_class' => 'SubPanelRemoveButton',
+    'module' => 'Contacts',
+    'width' => '5%',
+    'default' => true,
+  ),
+  'first_name' => 
+  array (
+    'name' => 'first_name',
+    'usage' => 'query_only',
+  ),
+  'last_name' => 
+  array (
+    'name' => 'last_name',
+    'usage' => 'query_only',
+  ),
+  'salutation' => 
+  array (
+    'name' => 'salutation',
+    'usage' => 'query_only',
+  ),
+  'contact_id' => 
+  array (
+    'usage' => 'query_only',
+  ),
+);
