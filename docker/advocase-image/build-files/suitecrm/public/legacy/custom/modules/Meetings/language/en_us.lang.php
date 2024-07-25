e79152244f6bd877b0426106b99ceaf6bb706dc4
tamaradesmith <tdesmith81@gmail.com>
2024-07-25 08:05:44 -0700

    feat/asa-256: meeting create contact default state

--- docker/advocase-image/build-files/suitecrm/public/legacy/custom/modules/Meetings/language/en_us.lang.php
+++ docker/advocase-image/build-files/suitecrm/public/legacy/custom/modules/Meetings/language/en_us.lang.php
@@ -7,4 +7,6 @@ if (!defined('sugarEntry') || !sugarEntry) {
 $mod_strings = array(
     'LBL_LIST_RELATED_TO' => 'Cases',
     'LBL_LIST_ASSIGNED_TO_NAME' => 'Assigned To',
+    'LBL_CREATE_INVITEE' => 'Create a New Contact',
+    'LBL_CREATE_CONTACT' => 'Create a New Contact',
 );
