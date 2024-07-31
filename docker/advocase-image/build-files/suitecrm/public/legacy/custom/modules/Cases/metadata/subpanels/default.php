590839ff5ec700d5eb7fad6ebae434fa4065bd24
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2024-07-30 17:22:40 -0700

    ASA-264 fix: communications sub panel

--- docker/advocase-image/build-files/suitecrm/public/legacy/custom/modules/Cases/metadata/subpanels/default.php
+++ docker/advocase-image/build-files/suitecrm/public/legacy/custom/modules/Cases/metadata/subpanels/default.php
@@ -1,54 +1,54 @@
 <?php
-// created: 2024-07-24 15:10:59
+// created: 2024-07-30 19:07:30
 $subpanel_layout['list_fields'] = array (
+  'date_entered' => 
+  array (
+    'type' => 'datetime',
+    'vname' => 'LBL_DATE_ENTERED',
+    'width' => '10%',
+    'default' => true,
+  ),
   'name' => 
   array (
     'name' => 'name',
     'vname' => 'LBL_LIST_NAME',
-    'sort_by' => 'last_name',
-    'sort_order' => 'asc',
     'widget_class' => 'SubPanelDetailViewLink',
     'module' => 'Contacts',
     'width' => '23%',
     'default' => true,
   ),
-  'advocase_contact_type_c' => 
-  array (
-    'type' => 'enum',
-    'default' => true,
-    'studio' => 'visible',
-    'vname' => 'LBL_ADVOCASE_CONTACT_TYPE',
-    'width' => '10%',
-  ),
-  'primary_address_city' => 
+  'advocase_case_city_c' => 
   array (
     'type' => 'varchar',
-    'vname' => 'LBL_PRIMARY_ADDRESS_CITY',
-    'width' => '10%',
     'default' => true,
+    'vname' => 'LBL_ADVOCASE_CASE_CITY',
+    'width' => '10%',
   ),
-  'advocase_contact_region_c' => 
+  'advocase_case_region_c' => 
   array (
     'type' => 'enum',
     'default' => true,
     'studio' => 'visible',
-    'vname' => 'LBL_ADVOCASE_CONTACT_REGION',
+    'vname' => 'LBL_ADVOCASE_CASE_REGION',
     'width' => '10%',
   ),
-  'phone_work' => 
+  'assigned_user_name' => 
   array (
-    'name' => 'phone_work',
-    'vname' => 'LBL_LIST_PHONE',
-    'width' => '15%',
+    'link' => true,
+    'type' => 'relate',
+    'vname' => 'LBL_ASSIGNED_TO_NAME',
+    'id' => 'ASSIGNED_USER_ID',
+    'width' => '10%',
     'default' => true,
+    'widget_class' => 'SubPanelDetailViewLink',
+    'target_module' => 'Users',
+    'target_record_key' => 'assigned_user_id',
   ),
-  'email1' => 
+  'status' => 
   array (
-    'name' => 'email1',
-    'vname' => 'LBL_LIST_EMAIL',
-    'widget_class' => 'SubPanelEmailLink',
-    'width' => '30%',
-    'sortable' => true,
+    'type' => 'dynamicenum',
+    'vname' => 'LBL_STATUS',
+    'width' => '10%',
     'default' => true,
   ),
   'edit_button' => 
@@ -82,7 +82,7 @@ $subpanel_layout['list_fields'] = array (
     'name' => 'salutation',
     'usage' => 'query_only',
   ),
-  'account_id' => 
+  'contact_id' => 
   array (
     'usage' => 'query_only',
   ),
