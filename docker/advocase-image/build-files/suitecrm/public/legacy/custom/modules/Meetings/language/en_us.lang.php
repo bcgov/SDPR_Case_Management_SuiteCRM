e39f28492d30c858fb01decf990bc5ef97ac95b5
Davis Qi <907225883wxl@gmail.com>
2024-07-31 16:25:36 -0700

    feat/update label text in create communication

--- docker/advocase-image/build-files/suitecrm/public/legacy/custom/modules/Meetings/language/en_us.lang.php
+++ docker/advocase-image/build-files/suitecrm/public/legacy/custom/modules/Meetings/language/en_us.lang.php
@@ -5,7 +5,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_LIST_RELATED_TO' => 'Cases',
+    'LBL_LIST_RELATED_TO' => 'Related to',
     'LBL_LIST_ASSIGNED_TO_NAME' => 'Assigned To',
     'LBL_CREATE_INVITEE' => 'Create a New Contact',
     'LBL_CREATE_CONTACT' => 'Create a New Contact',
