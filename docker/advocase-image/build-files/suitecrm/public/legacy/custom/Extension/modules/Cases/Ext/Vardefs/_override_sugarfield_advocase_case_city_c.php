615dbc46cbb2b47494ab6b7efe88499a3c82f78b
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2024-08-08 19:26:48 -0700

    ASA-276 chore: cleanup files

--- docker/advocase-image/build-files/suitecrm/public/legacy/custom/Extension/modules/Cases/Ext/Vardefs/_override_sugarfield_advocase_case_city_c.php
+++
@@ -1,6 +0,0 @@
-<?php
- // created: 2024-05-17 23:59:03
-$dictionary['Case']['fields']['advocase_case_city_c']['inline_edit']='1';
-$dictionary['Case']['fields']['advocase_case_city_c']['labelValue']='City for Case';
-
- ?>
